fa4c59b544fcca8e2898bb3ff3c68d8cc8861107
obay <obaymardini@gmail.com>
2016-09-16 05:22:21 +0200

    add two button to show user table

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var hb = require('express-handlebars');
 var queryDB = require('./queryDB');
 var redis = require('redis');
 var clientRE = redis.createClient({
-  host: 'localhost',
-  port: 6379
+    host: 'localhost',
+    port: 6379
 });
 var hb = require('express-handlebars');
 app.engine('handlebars', hb());
@@ -37,7 +37,7 @@ app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 //app.use(cookiesParser());
@@ -45,91 +45,90 @@ app.use(bodyParser.urlencoded({
 app.use(staticProjects);
 
 app.post('/filter', function(req, res) {
-  queryDB.filterTable(req.body.city, req.body.color, req.body.age).then(function(val){
-    res.render('hello', {
-      layout: 'main',
-      status: '/logOut',
-      button: 'log out',
-      data: val,
-      city: city,
-      age: age,
-      color: color
+    queryDB.filterTable(req.body.city, req.body.color, req.body.age).then(function(val) {
+        res.render('hello', {
+            layout: 'main',
+            status: '/logOut',
+            button: 'log out',
+            data: val,
+            city: city,
+            age: age,
+            color: color
+        });
     });
-  });
 });
 
-app.get('/showTable', function(req,res) {
+app.get('/showTable', function(req, res) {
     try {
         queryDB.checkUserAuth(req.session.user.email, req.session.user.password, function(err, data) {
-            if(err){
+            if (err) {
                 res.redirect('/register');
             } else {
                 queryDB.joinTables().then(function(val) {
-                  color = val.map(function(record){
-                    return record.color;
-                  }).filter(function(item, index, array){
-                    return array.indexOf(item) === index;
-                  });
+                    color = val.map(function(record) {
+                        return record.color;
+                    }).filter(function(item, index, array) {
+                        return array.indexOf(item) === index;
+                    });
 
-                  city = val.map(function(record){
-                    return record.city
-                  }).filter(function(item, index, array){
-                    return array.indexOf(item) === index;
-                  });
+                    city = val.map(function(record) {
+                        return record.city
+                    }).filter(function(item, index, array) {
+                        return array.indexOf(item) === index;
+                    });
 
-                  age = val.map(function(record){
-                    return record.age;
-                  }).filter(function(item, index, array){
-                    return array.indexOf(item) === index;
-                  });
+                    age = val.map(function(record) {
+                        return record.age;
+                    }).filter(function(item, index, array) {
+                        return array.indexOf(item) === index;
+                    });
 
-                  res.render('hello', {
-                      layout: 'main',
-                      button: 'log out',
-                      status: '/louOut',
+                    res.render('hello', {
+                        layout: 'main',
+                        button: 'log out',
+                        status: '/louOut',
                         data: val,
                         city: city,
                         color: color,
-                        age: age
-                      ,function(err, results){
-                        res.send(results)
-                      }
-                  }
-                  );
-              });
+                        age: age,
+                        function(err, results) {
+                            res.send(results)
+                        }
+                    });
+                });
 
             }
 
         })
-    } catch(err) {
+    } catch (err) {
         res.redirect('/register')
     }
 
-  });
+});
 
-app.get('/logIn', function(req, res){
-        res.render('logInForm', {
-            layout: 'main',
-            button: 'log in',
-            status: '/logIn',
-            message: ''
-        });
+app.get('/logIn', function(req, res) {
+    res.render('logInForm', {
+        layout: 'main',
+        button: 'log in',
+        status: '/logIn',
+        message: ''
+    });
 });
 
 app.post('/logInForm', function(req, res) {
     var body = req.body;
     //queryDB.checkUserAuth(body.email, res);
-    queryDB.checkUserAuth(body.email,body.password, function(err, data) {
-        if(err){
+    queryDB.checkUserAuth(body.email, body.password, function(err, data) {
+        if (err) {
             res.render('logInForm', {
                 message: 'you email or password are not correct'
             });
         }
-        if(data){
+        if (data) {
             req.session.user = {
-              email: body.email,
-              password: body.password,
-              id: data
+                email: body.email,
+                password: body.password,
+                id: data
             };
             res.redirect('/showTable');
         }
@@ -137,34 +136,34 @@ app.post('/logInForm', function(req, res) {
 });
 
 app.post('/editProfile', function(req, res) {
-    var columns = ['firstname', 'lastname' ,'email','password', 'age','city','url','color'];
+    var columns = ['firstname', 'lastname', 'email', 'password', 'age', 'city', 'url', 'color'];
     var registerationObj = {};
     var profileObj = {};
 
-    columns.forEach(function(item,index,array) {
-        if(index < 4 && req.body[item]) {
+    columns.forEach(function(item, index, array) {
+        if (index < 4 && req.body[item]) {
             registerationObj[item] = req.body[item];
-        } else if(index >= 4 && req.body[item]){
+        } else if (index >= 4 && req.body[item]) {
             profileObj[item] = req.body[item];
         }
     });
 
-    if(Object.keys(registerationObj).length !== 0){
-        queryDB.updateRecord(registerationObj, req.session, 'registration', function(err,data){
-            if(err){
+    if (Object.keys(registerationObj).length !== 0) {
+        queryDB.updateRecord(registerationObj, req.session, 'registration', function(err, data) {
+            if (err) {
                 res.status(404);
             }
-            if(data){
+            if (data) {
                 console.log('changes to profileTabel are DONE')
             }
         });
     }
-    if(Object.keys(profileObj).length !== 0) {
-        queryDB.updateRecord(profileObj,req.session, 'user_profiles', function(err,data){
-            if(err){
+    if (Object.keys(profileObj).length !== 0) {
+        queryDB.updateRecord(profileObj, req.session, 'user_profiles', function(err, data) {
+            if (err) {
                 res.status(404);
             }
-            if(data){
+            if (data) {
                 console.log('changes to profileTabel are DONE')
             }
         });
@@ -172,8 +171,8 @@ app.post('/editProfile', function(req, res) {
     }
 });
 
-app.get('/editProfile', function(req, res){
-    res.render('editProfile',{
+app.get('/editProfile', function(req, res) {
+    res.render('editProfile', {
         layout: "main",
         status: '/logOut',
         button: 'log in'
@@ -182,30 +181,30 @@ app.get('/editProfile', function(req, res){
 });
 
 app.post('/name', function(req, res) {
-  var body = req.body;
-  if(!body.firstname){
-    res.redirect('/signUp')
-  }else {
-    /*
-    res.cookie('name', body.firstname);
-    res.cookie('lastName', body.lastname);
-    */
-    queryDB.sendQuery(body.firstname, body.lastname).then(function(val) {
-      req.session.user = {
-        firstName: body.firstname,
-        lastName: body.lastname,
-        id: val
-    };
-      res.redirect('/newForm.html');
-    }).catch(function(err) {
-      res.sendStatus(500);
-    });
-    // will take care of the cookies later
-    //console.log(req.cookies);
-  }
+    var body = req.body;
+    if (!body.firstname) {
+        res.redirect('/signUp')
+    } else {
+        /*
+        res.cookie('name', body.firstname);
+        res.cookie('lastName', body.lastname);
+        */
+        queryDB.sendQuery(body.firstname, body.lastname).then(function(val) {
+            req.session.user = {
+                firstName: body.firstname,
+                lastName: body.lastname,
+                id: val
+            };
+            res.redirect('/newForm.html');
+        }).catch(function(err) {
+            res.sendStatus(500);
+        });
+        // will take care of the cookies later
+        //console.log(req.cookies);
+    }
 });
 
-app.get('/signUp', function(req, res){
+app.get('/signUp', function(req, res) {
     res.render('signUp', {
         layout: 'main',
         status: '/logIn',
@@ -213,94 +212,103 @@ app.get('/signUp', function(req, res){
     })
 })
 app.post('/registrationForm', function(req, res) {
-  var body = req.body;
-  if(!body.firstname || !body.password || !body.lastname || !body.email){
-    res.redirect('/signUp');
-  }else {
-    crypt.hashPassword(body.password).then(function(hash){
-        return queryDB.signUp(body.firstname,body.lastname, body.email, hash);
-    }).then(function(id){
-        req.session.user = {
-          name: body.name,
-          email: body.email,
-          password: body.password,
-          id: id
-        }
-       res.redirect('/newForm.html');
-   });
-  }
+    var body = req.body;
+    if (!body.firstname || !body.password || !body.lastname || !body.email) {
+        res.redirect('/signUp');
+    } else {
+        crypt.hashPassword(body.password).then(function(hash) {
+            return queryDB.signUp(body.firstname, body.lastname, body.email, hash);
+        }).then(function(id) {
+            req.session.user = {
+                name: body.name,
+                email: body.email,
+                password: body.password,
+                id: id
+            }
+            res.redirect('/newForm.html');
+        });
+    }
 });
 
-app.post('/userProfile', function(req, res){
-  var body = req.body;
-   queryDB.makeUserProfileTable(body.age, body.city, body.url, body.color, req.session.user.id).then(function(val) {
-    res.redirect('/showTable')
-  });
+app.post('/userProfile', function(req, res) {
+    var body = req.body;
+    queryDB.makeUserProfileTable(body.age, body.city, body.url, body.color, req.session.user.id).then(function(val) {
+        res.redirect('/showTable')
+    });
 });
 
 // recieve a request
-app.get('/helloWorld', function(req, res){
-  res.send('<!doctype html><title>Hello World!</title><p>Hello World!</html>');
+app.get('/helloWorld', function(req, res) {
+    res.send('<!doctype html><title>Hello World!</title><p>Hello World!</html>');
 });
 
-app.get('/logout', function(req, res){
-  req.session.destroy(function(err){
-      res.clearCookie('connect.sid');
-      res.redirect('/showTable');
-      console.log(req.session)
-  });
+app.get('/logout', function(req, res) {
+    req.session.destroy(function(err) {
+        res.clearCookie('connect.sid');
+        res.redirect('/showTable');
+        console.log(req.session)
+    });
 
 });
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
     res.render('mainPage', {
-      layout: 'main',
-      button: 'log in',
-      status: '/logIn',
-      details: {name: req.params.project, description: req.params.project + ' is super fun', link: '/' + req.params.project}
+        layout: 'main',
+        button: 'log in',
+        status: '/logIn',
+        details: {
+            name: req.params.project,
+            description: req.params.project + ' is super fun',
+            link: '/' + req.params.project
+        }
     });
 });
 
-app.post('/user', function(req, res){
-  console.log(req);
-  res.write('hello world');
-  res.end();
+app.post('/user', function(req, res) {
+    console.log(req);
+    res.write('hello world');
+    res.end();
 });
 
-app.get('/twitter', function(req, res){
-  new Promise(function(resolve, reject) {
-    clientRE.get('twitterDATA', function(err, data){
-      if(err){
-        console.log(err);
-      }
-      if(data){
-        console.log('get')
-        resolve(JSON.parse(data));
-      }
-      resolve(search.getTokens('theOnion'))
-    });
+app.get('/twitter', function(req, res) {
+    new Promise(function(resolve, reject) {
+        clientRE.get('twitterDATA', function(err, data) {
+            if (err) {
+                console.log(err);
+            }
+            if (data) {
+                console.log('get')
+                resolve(JSON.parse(data));
+            }
+            resolve(search.getTokens('theOnion'))
+        });
 
-  }).then(function(val){
-      return search.search(val);
-    }).then(function(val){
-      var twittsArray = val.map(function(twit){
-        twit = twit.text.split('http');
-        return {'url': twit[0], 'title': twit[0]}
-      });
-      return twittsArray;
-    }).then(function(val){
-    res.setHeader('Content-Type', 'Application/json')
-    res.json(val);
-  }).catch(function(err){
-    console.log(err)
-    res.status(500).send({error: 'something failed in the server'});
-  });
+    }).then(function(val) {
+        return search.search(val);
+    }).then(function(val) {
+        var twittsArray = val.map(function(twit) {
+            twit = twit.text.split('http');
+            return {
+                'url': twit[0],
+                'title': twit[0]
+            }
+        });
+        return twittsArray;
+    }).then(function(val) {
+        res.setHeader('Content-Type', 'Application/json')
+        res.json(val);
+    }).catch(function(err) {
+        console.log(err)
+        res.status(500).send({
+            error: 'something failed in the server'
+        });
+    });
 });
 
 app.use(function(req, res) {
-  if(!req.body.firstname) {
-    res.redirect('/signUp');
-  }
+    if (!req.body.firstname) {
+        res.redirect('/signUp');
+    }
 });
 
 app.listen(8080);
